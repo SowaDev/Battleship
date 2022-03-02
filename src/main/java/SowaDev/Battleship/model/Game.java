1c06ebdb28a147086cc91ae62f42bc4e20b347f5
SowaDev <piotrsowa@yahoo.com>
2022-03-02 15:21:05 +0100

    added String attribute playerTurn to differentiate whose turn it is, and added a constructor

--- src/main/java/SowaDev/Battleship/model/Game.java
+++ src/main/java/SowaDev/Battleship/model/Game.java
@@ -1,13 +1,22 @@
 package SowaDev.Battleship.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import java.util.UUID;
 
 @Data
-@AllArgsConstructor
 public class Game {
     private String gameId;
+//    private UUID gameId;
     private Player player1;
     private Player player2;
     private GameStatus gameStatus;
+    private String playerTurn;
+//    private UUID playerTurn;
+
+    public Game(String gameId, Player player1, Player player2, GameStatus gameStatus){
+        this.gameId = gameId;
+        this.player1 = player1;
+        this.player2 = player2;
+        this.gameStatus = gameStatus;
+    }
 }
