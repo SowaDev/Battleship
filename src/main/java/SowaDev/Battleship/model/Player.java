123c415ff286c5b215f0835b23ff330c03f66f08
SowaDev <piotrsowa@yahoo.com>
2022-03-02 15:18:14 +0100

    added additional String attribute playerId

--- src/main/java/SowaDev/Battleship/model/Player.java
+++ src/main/java/SowaDev/Battleship/model/Player.java
@@ -5,16 +5,20 @@ import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Getter
 @Setter
 public class Player {
     private String name;
+//    private UUID playerId;
+    private String playerId;
 //    private Fleet fleet;
     private List<Ship> fleet = new ArrayList<>();
     private Grid grid;
 
-    public Player(List<Ship> fleet, Grid grid){
+    public Player(String playerId, List<Ship> fleet, Grid grid){
+        this.playerId = playerId;
         this.fleet = fleet;
         this.grid = grid;
     }
