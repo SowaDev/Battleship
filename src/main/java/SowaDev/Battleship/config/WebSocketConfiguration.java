c6f6eee5822a863cf0358c2dd23807a1d459374a
SowaDev <piotrsowa@yahoo.com>
2022-10-10 20:31:38 +0200

    removed unused import

--- src/main/java/SowaDev/Battleship/config/WebSocketConfiguration.java
+++ src/main/java/SowaDev/Battleship/config/WebSocketConfiguration.java
@@ -2,10 +2,10 @@ package SowaDev.Battleship.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
