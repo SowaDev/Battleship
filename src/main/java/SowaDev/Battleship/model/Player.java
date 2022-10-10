8d2d5d96db66d3ac1fa1d7d61ff50977dffc21cc
SowaDev <piotrsowa@yahoo.com>
2022-10-10 20:34:45 +0200

    set default value on name property to an empty string

--- src/main/java/SowaDev/Battleship/model/Player.java
+++ src/main/java/SowaDev/Battleship/model/Player.java
@@ -3,7 +3,6 @@ package SowaDev.Battleship.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,10 +13,11 @@ public class Player {
 //    private UUID playerId;
     private String playerId;
 //    private Fleet fleet;
-    private List<Ship> fleet = new ArrayList<>();
+    private List<Ship> fleet;
     private Grid grid;
 
     public Player(String playerId, List<Ship> fleet, Grid grid){
+        this.name = "";
         this.playerId = playerId;
         this.fleet = fleet;
         this.grid = grid;
