d7d1838c5a35b013facbe58ddd35f0b6fe7e39b0
SowaDev <piotrsowa@yahoo.com>
2022-10-10 22:24:27 +0200

    feat: app gets user data and set is as a state during first render

--- src/front/src/Components/App/App.js
+++ src/front/src/Components/App/App.js
@@ -1,9 +1,21 @@
+import {useState, useEffect} from 'react'
+import {fetchUser} from '../../Utils.js'
 import './App.css';
 
+const url = 'http://localhost:8080/'
+
 function App() {
+  const [userData, setUserData] = useState({})
+
+  useEffect(() => {
+    fetchUser(url).then(user => {
+      setUserData(user)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <p>hello</p>
+      <p>{userData.playerId}</p>
     </div>
   );
 }
