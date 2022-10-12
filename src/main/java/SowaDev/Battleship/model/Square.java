9a68c09d4b82d48eb8db1831ef2ce442372ee112
SowaDev <piotrsowa@yahoo.com>
2022-10-12 14:08:06 +0200

    refactor: put x, y variable into one piece -> coordinates

--- src/main/java/SowaDev/Battleship/model/Square.java
+++ src/main/java/SowaDev/Battleship/model/Square.java
@@ -6,13 +6,13 @@ import lombok.Setter;
 @Setter
 @Getter
 public class Square {
-    private int x, y;
+//    private int x, y;
+    private Coordinates coordinates;
     private Ship ship;
     private boolean wasShot;
     private boolean isRestricted;
 
     public Square(int x, int y){
-        this.x = x;
-        this.y = y;
+        this.coordinates = new Coordinates(x, y);
     }
 }
