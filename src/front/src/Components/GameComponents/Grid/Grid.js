367c21205b78bfcf3cce21262070bcbf8ec8069e
Tsukahara Bokuden <piotrsowa@yahoo.com>
2023-01-26 17:41:56 +0100

    feat: created basic display for the game

--- src/front/src/Components/GameComponents/Grid/Grid.js
+++ src/front/src/Components/GameComponents/Grid/Grid.js
@@ -1,24 +1,23 @@
 import React from 'react'
-import Square from '../../HomeComponents/Square/Square'
+import Square from '../../GameComponents/Square/Square'
+import './Grid.css'
 
 export default function Grid({ battleMap }) {
   return (
-    <div className="BattleMap">
-      {battleMap.map((row, i) => {
-        return (
-          <div className="row" key={`row${i}`}>
-            {row.map((square, j) => {
-              return (
-                <Square
-                  key={`${i}, ${j}`}
-                  coordinates={square.coordinates}
-                  color={square.color}
-                />
-              )
-            })}
-          </div>
-        )
-      })}
+    <div className="GameGrid">
+      <div className="GameBattleMap">
+        {battleMap.map((row, i) => {
+          return (
+            <div className="row" key={`row${i}`}>
+              {row.map((square, j) => {
+                return (
+                  <Square key={`${i}, ${j}`} coordinates={square.coordinates} />
+                )
+              })}
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
--- src/front/src/Pages/Game/Game.js
+++ src/front/src/Pages/Game/Game.js
@@ -1,14 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import { fetchGame, createOpponentBattleMap } from '../../Utils'
+import Grid from '../../Components/GameComponents/Grid/Grid'
+import Square from '../../Components/GameComponents/Square/Square'
+import './Game.css'
 
 export default function Game() {
   const { state } = useLocation()
-  const [game, setGame] = useState(state)
+  const { userId, userName } = state
+  const [userBattleMap, setUserBattleMap] = useState([])
+  const [opponentBattleMap, setOpponentBattleMap] = useState([])
+  const [opponentName, setOpponentName] = useState('')
+  const [gameStatus, setGameStatus] = useState('')
+
+  useEffect(() => {
+    console.log(userId)
+    fetchGame().then((game) => {
+      setUserBattleMap(
+        game.players.find((user) => user && user.playerId === userId).grid
+          .battleMap
+      )
+      if (game.gameStatus === 'NEW') {
+        let opon = createOpponentBattleMap()
+        console.log(opon)
+        setOpponentBattleMap(createOpponentBattleMap())
+      } else {
+        setOpponentBattleMap(mountOpponentBattleMap())
+      }
+    })
+  }, [])
+
+  const mountOpponentBattleMap = (grid) => {}
 
   return (
-    <div>
-      <h1>Game</h1>
-      <div>GAME</div>
+    <div className="Game">
+      <h1>{userId}</h1>
+      <div className="Map">
+        <Grid battleMap={userBattleMap} />
+        <Grid battleMap={opponentBattleMap} />
+      </div>
     </div>
   )
 }
