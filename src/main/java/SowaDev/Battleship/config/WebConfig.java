ebdec833be511046c6cfe8b62d44c875c5d152b9
Tsukahara Bokuden <piotrsowa@yahoo.com>
2023-01-28 19:25:37 +0100

    fix: fixed cors error that ocurred when websockets were implemented in the client

--- src/main/java/SowaDev/Battleship/config/WebConfig.java
+++ src/main/java/SowaDev/Battleship/config/WebConfig.java
@@ -1,2 +1,16 @@
-package SowaDev.Battleship.config;public class WebConfig {
+package SowaDev.Battleship.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+                .allowCredentials(true);
+    }
 }
