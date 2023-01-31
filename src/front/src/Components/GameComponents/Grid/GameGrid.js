c98ce629ca0985793fdf6d4214e397e74b4adbbf
Tsukahara Bokuden <piotrsowa@yahoo.com>
2023-01-31 15:12:35 +0100

    feat: adjusted display to show user's grid much smaller than opponents

--- src/front/src/Components/GameComponents/Grid/GameGrid.js
+++ src/front/src/Components/GameComponents/Grid/GameGrid.js
@@ -4,27 +4,31 @@ import './GameGrid.css'
 
 export default function Grid({ battleMap, opponent, shoot }) {
   return (
-    <div className="GameGrid">
-      <div className="GameBattleMap">
-        {battleMap.map((row, i) => {
-          return (
-            <div className="row" key={`row${i}`}>
-              {row.map((square, j) => {
-                return (
-                  <Square
-                    key={`${i}, ${j}`}
-                    coordinates={square.coordinates}
-                    status={square.status}
-                    ship={opponent ? null : square.ship}
-                    opponent={opponent}
-                    shoot={shoot}
-                  />
-                )
-              })}
-            </div>
-          )
-        })}
-      </div>
+    <div
+      className="GameGrid"
+      style={{
+        height: opponent ? '30vw' : '20vw',
+        width: opponent ? '30vw' : '20vw',
+      }}
+    >
+      {battleMap.map((row, i) => {
+        return (
+          <div className="row" key={`row${i}`}>
+            {row.map((square, j) => {
+              return (
+                <Square
+                  key={`${i}, ${j}`}
+                  coordinates={square.coordinates}
+                  status={square.status}
+                  ship={opponent ? null : square.ship}
+                  opponent={opponent}
+                  shoot={shoot}
+                />
+              )
+            })}
+          </div>
+        )
+      })}
     </div>
   )
 }
