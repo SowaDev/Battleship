043dbe46dff94516096a837d9c23db0a4d1e1def
Tsukahara Bokuden <piotrsowa@yahoo.com>
2023-01-31 15:15:59 +0100

    fix: changed square colors under different circumstances

--- src/front/src/Components/GameComponents/Square/GameSquare.js
+++ src/front/src/Components/GameComponents/Square/GameSquare.js
@@ -3,10 +3,12 @@ import './GameSquare.css'
 
 export default function Square({ coordinates, status, opponent, ship, shoot }) {
   const setColor = () => {
-    if (status === 'NOT_SHOT' && ship) return 'gray'
-    else if (status === 'NOT_SHOT') return opponent ? 'gray' : 'lightblue'
-    else if (status === 'MISS') return opponent ? 'lightblue' : 'orange'
-    else if (status === 'HIT') return 'red'
+    if (status === 'NOT_SHOT' && ship) return 'rgba(230, 202, 50, 0.8)'
+    else if (status === 'NOT_SHOT')
+      return opponent ? 'rgba(108, 122, 137, 0.6)' : 'rgba(53, 119, 122, 0.3)'
+    else if (status === 'MISS')
+      return opponent ? 'rgba(12, 164, 232, 0.6)' : 'rgba(12, 164, 232, 0.6)'
+    else if (status === 'HIT') return 'rgba(250, 0, 0, 0.6)'
   }
 
   const handleClick = () => {
