86bfbe526df8f8e0de93f58f6d15535861d328bc
Tsukahara Bokuden <piotrsowa@yahoo.com>
2023-02-07 22:10:31 +0100

    feat: added functionality that highlights squares when player is choosing where to shoot

--- src/front/src/Components/GameComponents/Square/GameSquare.js
+++ src/front/src/Components/GameComponents/Square/GameSquare.js
@@ -5,7 +5,7 @@ export default function Square({ coordinates, status, opponent, ship, shoot }) {
   const setColor = () => {
     if (status === 'NOT_SHOT' && ship) return 'rgba(230, 202, 50, 0.8)'
     else if (status === 'NOT_SHOT')
-      return opponent ? 'rgba(108, 122, 137, 0.6)' : 'rgba(53, 119, 122, 0.3)'
+      return opponent ? 'rgba(0, 0, 0, 0.2)' : 'rgba(0, 0, 0, 0.2)'
     else if (status === 'MISS')
       return opponent ? 'rgba(12, 164, 232, 0.6)' : 'rgba(12, 164, 232, 0.6)'
     else if (status === 'HIT') return 'rgba(250, 0, 0, 0.6)'
@@ -15,6 +15,18 @@ export default function Square({ coordinates, status, opponent, ship, shoot }) {
     shoot(coordinates)
   }
 
+  const handleMouseEnter = (e) => {
+    if (opponent && status === 'NOT_SHOT') {
+      e.target.style.backgroundColor = 'rgba(230, 202, 50, 0.8)'
+    }
+  }
+
+  const handleMouseLeave = (e) => {
+    if (opponent && status === 'NOT_SHOT') {
+      e.target.style.backgroundColor = 'rgba(0, 0, 0, 0.2)'
+    }
+  }
+
   return (
     <div
       className="GameSquare"
@@ -22,6 +34,8 @@ export default function Square({ coordinates, status, opponent, ship, shoot }) {
         backgroundColor: setColor(),
       }}
       onClick={opponent ? handleClick : null}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     ></div>
   )
 }
