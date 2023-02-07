514eaeb1ea42a7a5c2fc4a01218229e18ff304a8
Tsukahara Bokuden <piotrsowa@yahoo.com>
2023-02-07 22:34:30 +0100

    refactor: rewritten logic to use useState and useEffect hooks

--- src/front/src/Components/GameComponents/Square/GameSquare.js
+++ src/front/src/Components/GameComponents/Square/GameSquare.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './GameSquare.css'
 
 export default function Square({ coordinates, status, opponent, ship, shoot }) {
-  const setColor = () => {
-    if (status === 'NOT_SHOT' && ship) return 'rgba(230, 202, 50, 0.8)'
+  const [color, setColor] = useState('')
+
+  useEffect(() => {
+    if (status === 'NOT_SHOT' && ship) setColor('rgba(230, 202, 50, 0.8)')
     else if (status === 'NOT_SHOT')
-      return opponent ? 'rgba(0, 0, 0, 0.2)' : 'rgba(0, 0, 0, 0.2)'
+      setColor(opponent ? 'rgba(0, 0, 0, 0.2)' : 'rgba(0, 0, 0, 0.2)')
     else if (status === 'MISS')
-      return opponent ? 'rgba(12, 164, 232, 0.6)' : 'rgba(12, 164, 232, 0.6)'
-    else if (status === 'HIT') return 'rgba(250, 0, 0, 0.6)'
-  }
+      setColor(opponent ? 'rgba(12, 164, 232, 0.6)' : 'rgba(12, 164, 232, 0.6)')
+    else if (status === 'HIT') setColor('rgba(250, 0, 0, 0.6)')
+  }, [status])
 
   const handleClick = () => {
     shoot(coordinates)
@@ -31,7 +33,7 @@ export default function Square({ coordinates, status, opponent, ship, shoot }) {
     <div
       className="GameSquare"
       style={{
-        backgroundColor: setColor(),
+        backgroundColor: color,
       }}
       onClick={opponent ? handleClick : null}
       onMouseEnter={handleMouseEnter}
