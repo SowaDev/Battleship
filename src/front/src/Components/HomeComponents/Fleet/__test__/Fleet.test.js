8dfb790c8865a0a33472054eccefdda80c89e89e
Tsukahara Bokuden <piotrsowa@yahoo.com>
2023-02-10 20:24:18 +0100

    feat: added more tests for Fleet component

--- src/front/src/Components/HomeComponents/Fleet/__test__/Fleet.test.js
+++ src/front/src/Components/HomeComponents/Fleet/__test__/Fleet.test.js
@@ -1,7 +1,8 @@
-import { render, screen } from '@testing-library/react'
+import React from 'react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Fleet from '../Fleet'
 
-const ships = [
+const mockShips = [
   {
     name: 'AircraftCarrier',
     length: 5,
@@ -24,8 +25,25 @@ const ships = [
   },
 ]
 
+const setSelectedShip = jest.fn()
+
 test('render', async () => {
-  render(<Fleet ships={ships} />)
+  render(<Fleet ships={mockShips} />)
   const fleet = await screen.findByTestId('fleet')
   expect(fleet).toBeInTheDocument()
+})
+
+test('renders the correct number of ships', () => {
+  render(<Fleet ships={mockShips} />)
+  const ships = screen.getAllByRole('button')
+  expect(ships.length).toBe(mockShips.length)
+})
+
+test('calls setSelectedShip correctly', () => {
+  const setSelectedShip = jest.fn()
+  render(<Fleet ships={mockShips} setSelectedShip={setSelectedShip} />)
+  const ship = screen.getByText(mockShips[0].name)
+
+  fireEvent.click(ship)
+  expect(setSelectedShip).toHaveBeenCalledWith(mockShips[0])
 })
