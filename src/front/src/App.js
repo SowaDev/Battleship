a0268dd3ead0a26741f62f3a676346ab0be8c35c
Tsukahara Bokuden <piotrsowa@yahoo.com>
2023-02-10 22:45:38 +0100

    feat: wrapped routes with ColorContext Provider

--- src/front/src/App.js
+++ src/front/src/App.js
@@ -1,13 +1,18 @@
-// import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home/Home'
 import Game from './Pages/Game/Game'
+import { ColorContext, colors } from './ColorContext'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/game" element={<Game />} />
-    </Routes>
+    <BrowserRouter>
+      <ColorContext.Provider value={colors}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<Game />} />
+        </Routes>
+      </ColorContext.Provider>
+    </BrowserRouter>
   )
 }
