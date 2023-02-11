058ac9a6db6ec03bfc3d3e7729cdd7d771935c9d
Tsukahara Bokuden <piotrsowa@yahoo.com>
2023-02-11 23:41:40 +0100

    fix: fixed checking condition for transitioning the button

--- src/front/src/Components/HomeComponents/ButtonLink/ButtonLink.js
+++ src/front/src/Components/HomeComponents/ButtonLink/ButtonLink.js
@@ -12,14 +12,14 @@ export default function ButtonLink({
   const navigate = useNavigate()
 
   const handleEnter = (e) => {
-    if (fleetReady) {
+    if (fleetReady && userName !== '') {
       e.target.style.transform = 'scale(1.1)'
       e.target.style.boxShadow = '2px 4px 50px 0 rgba(0, 0, 0, 0.25)'
     }
   }
 
   const handleLeave = (e) => {
-    if (fleetReady) {
+    if (fleetReady && userName !== '') {
       e.target.style.transform = 'scale(1)'
       e.target.style.boxShadow = '2px 4px 10px 0 rgba(0, 0, 0, 0.25)'
     }
