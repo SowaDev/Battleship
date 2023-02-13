289c2b3224eae362f84e8e1ca1594a20b2ce0773
Tsukahara Bokuden <piotrsowa@yahoo.com>
2023-02-13 14:01:04 +0100

    refactor: slightly changed displayed color

--- src/front/src/Components/GameComponents/TurnBox/TurnBox.js
+++ src/front/src/Components/GameComponents/TurnBox/TurnBox.js
@@ -4,6 +4,7 @@ import './TurnBox.css'
 
 export default function TurnBox({ isUserTurn, gameStatus }) {
   const [text, setText] = useState('')
+
   useEffect(() => {
     setText(pickRightInfo(isUserTurn, gameStatus))
   }, [isUserTurn, gameStatus])
@@ -14,17 +15,17 @@ export default function TurnBox({ isUserTurn, gameStatus }) {
     else if (gameStatus === 'FINISHED') flash3times(isUserTurn, 500)
   }, [isUserTurn, gameStatus])
 
-  const { transparentBlack, golden } = useContext(ColorContext)
+  const { transparentBlack, goldenNotTrans } = useContext(ColorContext)
   const boxRef = useRef(null)
 
-  const pickRightInfo = (isUserTurn, gameStatus) => {
+  const pickRightInfo = () => {
     if (gameStatus === 'NEW') return 'Awaiting other player...'
     else if (gameStatus === 'IN_PROGRESS')
       return isUserTurn ? 'Your Turn' : 'Opponent Turn'
     else return isUserTurn ? "You've won" : "You've lost"
   }
 
-  const flash3times = async (isUserTurn, ms) => {
+  const flash3times = async (ms) => {
     boxRef.current.style.transition = `background-color ${ms}ms`
     for (let i = 0; i < 3; i++) {
       changeBackgroundColor(isUserTurn)
@@ -38,23 +39,26 @@ export default function TurnBox({ isUserTurn, gameStatus }) {
     }
   }
 
-  const flashWithColor = async (isUserTurn, ms) => {
+  const flashWithColor = async (ms) => {
     changeBackgroundColor(isUserTurn)
     setTimeout(() => {
       boxRef.current.style.background = transparentBlack
     }, ms)
   }
 
-  const changeBackgroundColor = (isUserTurn) => {
+  const changeBackgroundColor = () => {
     boxRef.current.style.background = isUserTurn
-      ? 'rgba(0, 250, 112, 0.6)'
-      : 'rgba(245, 133, 133, 0.6)'
+      ? 'rgba(0, 250, 112, 0.9)'
+      : 'rgba(255, 0, 0, 0.8)'
   }
 
-  const changeFontColor = (isUserTurn) => {
-    boxRef.current.style.color = isUserTurn
-      ? golden
-      : 'rgba(245, 133, 133, 0.836)'
+  const changeFontColor = () => {
+    if (gameStatus === 'NEW') boxRef.current.style.color = goldenNotTrans
+    else {
+      boxRef.current.style.color = isUserTurn
+        ? goldenNotTrans
+        : 'rgba(245, 133, 133, 0.836)'
+    }
   }
 
   return (
